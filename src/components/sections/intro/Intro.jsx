a1c0cf4592fd6db17d4b559d426ad7162048f868
dbsrn1110 <dbsrn1110@gmail.com>
2025-01-28 15:54:09 +0900

    reset.js 생성, 폴더뼈대구조,

---
+++ src/components/sections/intro/Intro.jsx
@@ -0,0 +1,11 @@
+const Intro = () => {
+    return (
+        <>
+            <section>
+                <h2>웹 프론트엔드 포트폴리오</h2>
+            </section>
+        </>
+    );
+};
+
+export default Intro;
